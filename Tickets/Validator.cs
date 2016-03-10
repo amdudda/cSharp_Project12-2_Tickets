145167ed271b1b76a9e931d7ae473cd077fbd8a6
amdudda <tk0654wm@minneapolis.edu>
2016-03-09 20:53:51 -0600

    add some validation code - not fully implemented

---
+++ Tickets/Validator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tickets
+{
+    public class Validator
+    {
+        public bool IsPresent(TextBox tbox)
+        {
+            if (tbox.Text == "")
+            {
+                MessageBox.Show(tbox.Tag + " cannot be empty.  Please enter a value.","Required Field");
+                tbox.Focus();
+                return false;
+            }
+            else return true;
+        }
+
+        public bool IsInteger(TextBox tbox) 
+        {
+            string toParse = tbox.Text;
+            short whatevs;
+            if (!(Int16.TryParse(toParse, out whatevs)) || whatevs < 1)
+            {
+                MessageBox.Show(tbox.Tag + " requires a positive integer greater than zero.", "Invalid Value");
+                tbox.Focus();
+                return false;
+            }
+            else return true;
+        }
+
+        // and a method to check that it's a valid time
+        public bool IsTime(TextBox tbox)
+        {
+            DateTime whatevs;
+            if (!(DateTime.TryParse(tbox.Text,out whatevs)))
+            {
+                MessageBox.Show(tbox.Tag + " does not appear to be a valid time.  Please check your entry.",
+                    "Invalid time");
+                return false;
+            }
+            else return true;
+        }
+
+        // checks that the start and end time allow for at least two time slots
+        // should really take three textboxes??
+        public bool AtLeastTwoSlots(DateTime anfang, DateTime endung, int minuten)
+        {
+            // we need to verify that the opening hours are at least the same length as the number of minutes.
+            // this ensures one slot at opening and one slot at closing.
+            TimeSpan timeDiff = endung - anfang;
+            if (timeDiff.Minutes >= minuten)
+            {
+                // TODO add alert message
+                return false;
+            }
+            else return true;
+        }
+    }
+}
---
+++ Tickets/frmOptions.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Tickets
+{
+    partial class frmOptions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtMinutes = new System.Windows.Forms.TextBox();
+            this.txtGuests = new System.Windows.Forms.TextBox();
+            this.txtStartTime = new System.Windows.Forms.TextBox();
+            this.txtEndTime = new System.Windows.Forms.TextBox();
+            this.txtFirstTicket = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "A.M. Dudda - Project 12.2 - Tickets";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Minutes per window:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Guests per window:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Start time:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "End time:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 143);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "First ticket number:";
+            // 
+            // txtMinutes
+            // 
+            this.txtMinutes.Location = new System.Drawing.Point(122, 28);
+            this.txtMinutes.Name = "txtMinutes";
+            this.txtMinutes.Size = new System.Drawing.Size(100, 20);
+            this.txtMinutes.TabIndex = 7;
+            this.txtMinutes.Tag = "Minutes";
+            this.txtMinutes.Text = "5";
+            // 
+            // txtGuests
+            // 
+            this.txtGuests.Location = new System.Drawing.Point(122, 56);
+            this.txtGuests.Name = "txtGuests";
+            this.txtGuests.Size = new System.Drawing.Size(100, 20);
+            this.txtGuests.TabIndex = 8;
+            this.txtGuests.Tag = "Guests";
+            this.txtGuests.Text = "5";
+            // 
+            // txtStartTime
+            // 
+            this.txtStartTime.Location = new System.Drawing.Point(122, 84);
+            this.txtStartTime.Name = "txtStartTime";
+            this.txtStartTime.Size = new System.Drawing.Size(100, 20);
+            this.txtStartTime.TabIndex = 9;
+            this.txtStartTime.Tag = "Start time";
+            this.txtStartTime.Text = "10:49 AM";
+            // 
+            // txtEndTime
+            // 
+            this.txtEndTime.Location = new System.Drawing.Point(122, 112);
+            this.txtEndTime.Name = "txtEndTime";
+            this.txtEndTime.Size = new System.Drawing.Size(100, 20);
+            this.txtEndTime.TabIndex = 10;
+            this.txtEndTime.Tag = "End time";
+            this.txtEndTime.Text = "2:49 PM";
+            // 
+            // txtFirstTicket
+            // 
+            this.txtFirstTicket.Location = new System.Drawing.Point(122, 140);
+            this.txtFirstTicket.Name = "txtFirstTicket";
+            this.txtFirstTicket.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstTicket.TabIndex = 11;
+            this.txtFirstTicket.Tag = "First ticket";
+            this.txtFirstTicket.Text = "1";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(122, 182);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(100, 23);
+            this.btnOK.TabIndex = 12;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // frmOptions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(241, 224);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtFirstTicket);
+            this.Controls.Add(this.txtEndTime);
+            this.Controls.Add(this.txtStartTime);
+            this.Controls.Add(this.txtGuests);
+            this.Controls.Add(this.txtMinutes);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmOptions";
+            this.Text = "Options";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtMinutes;
+        private System.Windows.Forms.TextBox txtGuests;
+        private System.Windows.Forms.TextBox txtStartTime;
+        private System.Windows.Forms.TextBox txtEndTime;
+        private System.Windows.Forms.TextBox txtFirstTicket;
+        private System.Windows.Forms.Button btnOK;
+    }
+}
